447c0207f24f91c8314ee1a79dae96032781dff4
haktan enes biçer <onkan_01@hotmail.com>
2018-06-12 09:22:10 +0300

    Proje dosyası ekle.

---
+++ Migrator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            foreach (var item in args)
+            {
+                if (item.Contains("Initial Catalog"))
+                {
+                    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(item);
+                    var dbname = builder.InitialCatalog;
+                    Console.WriteLine(dbname);
+                }
+            }
+           
+            
+
+        }
+    }
+}
