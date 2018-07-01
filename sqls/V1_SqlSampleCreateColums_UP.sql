9d1d4ac72bb6cab8747c93300c9fe137333f44a6
Evren Kayali <evren.kayali@readify.net>
2018-07-02 09:21:08 +1000

    sample scriots

---
+++ sqls/V1_SqlSampleCreateColums_UP.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [User]  
+
+   (Id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    FullName nvarchar(25) NOT NULL,  
+    Email nvarchar(30));
+
+GO
+
+CREATE Table [House]
+(
+  
+  
+			Id int IDENTITY(1,1) PRIMARY KEY (Id) NOT NULL,
+            Address nvarchar(25) NOT NULL,
+			UserId int ,
+			CONSTRAINT FK_UserHouse FOREIGN KEY (UserId)
+			REFERENCES [User](Id)
+   );
+GO
